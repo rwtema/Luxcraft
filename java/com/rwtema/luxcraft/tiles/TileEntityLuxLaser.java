70e4d03e1c7ade9e384e659e08d2932437ea73b2
rwtema <rwtema@gmail.com>
2014-02-23 19:46:14 +0000

    Improved laser code

--- java/com/rwtema/luxcraft/tiles/TileEntityLuxLaser.java
+++ java/com/rwtema/luxcraft/tiles/TileEntityLuxLaser.java
@@ -44,93 +44,113 @@ public class TileEntityLuxLaser extends TileEntity implements ILaserTile {
 
 		Pos inv = (new Pos(xCoord, yCoord, zCoord)).advance(BlockLuxLaser.getDirection(getBlockMetadata()).getOpposite());
 		TileEntity tile = worldObj.getTileEntity(inv.x, inv.y, inv.z);
-		if (tile == null || !(tile instanceof ILuxTransmitter))
-			return;
-
-		ILuxTransmitter transmit = (ILuxTransmitter) tile;
-
-		LaserType type = BlockLuxLaser.getLaser(this.getBlockMetadata());
-
-		if (path == null)
-			path = new LaserBeam(worldObj, xCoord, yCoord, zCoord, BlockLuxLaser.getDirection(this.getBlockMetadata()), type);
-
-		LuxStack pkt = transmit.getTransmissionPacket(getDir().getOpposite());
-
-		for (int i = 0; i < pkt.lux[i]; i++)
-			pkt.lux[i] = Math.min(pkt.lux[i], type.maxLux);
 
 		int[] newLaserLengths = new int[8];
 
-		Iterator<Pos> iter = path.iterator();
+		if (tile instanceof ILuxTransmitter) {
+			ILuxTransmitter transmit = (ILuxTransmitter) tile;
 
-		while (iter.hasNext()) {
-			boolean empty = true;
+			LaserType type = BlockLuxLaser.getLaser(this.getBlockMetadata());
 
-			for (int i = 0; i < pkt.lux.length; i++)
-				if (pkt.lux[i] > 0.0001F) {
-					empty = false;
-					newLaserLengths[i]++;
-				}
+			if (path == null)
+				path = new LaserBeam(worldObj, xCoord, yCoord, zCoord, BlockLuxLaser.getDirection(this.getBlockMetadata()), type);
 
-			if (empty) {
-				iter.remove();
-				break;
-			}
+			LuxStack pkt = transmit.getTransmissionPacket(getDir().getOpposite());
 
-			Pos p = iter.next();
-			TileEntity b = worldObj.getTileEntity(p.x, p.y, p.z);
-			if (b == null)
-				continue;
+			for (int i = 0; i < pkt.lux[i]; i++)
+				pkt.lux[i] = Math.min(pkt.lux[i], type.maxLux);
 
-			if (b instanceof ILaserActivated) {
-				((ILaserActivated) b).hit(path);
-			}
+			pkt = transmit.extractLux(pkt, Transfer.Simulate);
 
-			if (b instanceof ILuxLaserDivertor) {
-				List<Pos> lists = ((ILuxLaserDivertor) b).getAlternatePositions();
-				if (lists.size() > 0) {
-					LuxStack p2 = pkt.copy().div(lists.size());
-					for (Pos q : lists) {
-						if (inv.equals(q) || transmit.sameContainer(worldObj.getTileEntity(q.x, q.y, q.z))) {
-							pkt.extract(p2);
-							continue;
-						}
+			Iterator<Pos> iter = path.iterator();
 
-						ILuxContainer t = ((ILuxContainer) worldObj.getTileEntity(q.x, q.y, q.z));
+			while (iter.hasNext()) {
+				boolean empty = true;
 
-						LuxStack k = t.insertLux(p2, Transfer.Perform);
-						pkt.extract(k);
-						transmit.extractLux(k, Transfer.Perform);
+				for (int i = 0; i < pkt.lux.length; i++)
+					if (pkt.lux[i] > 0.0001F) {
+						empty = false;
+						newLaserLengths[i]++;
 					}
-				}
-			}
 
-			if (b instanceof ILuxContainer) {
-				if (inv.equals(p) || transmit.sameContainer(tile)) {
+				if (empty) {
 					iter.remove();
-					pkt = new LuxStack();
-
 					break;
 				}
 
-				LuxStack t = ((ILuxContainer) b).insertLux(pkt, Transfer.Perform);
-				pkt.extract(t);
-				transmit.extractLux(t, Transfer.Perform);
+				Pos p = iter.next();
+				TileEntity b = worldObj.getTileEntity(p.x, p.y, p.z);
+				if (b == null)
+					continue;
+
+				if (b instanceof ILaserActivated) {
+					((ILaserActivated) b).hit(path);
+				}
+
+				if (b instanceof ILuxLaserDivertor) {
+					List<Pos> lists = ((ILuxLaserDivertor) b).getAlternatePositions();
+					if (lists != null && lists.size() > 0) {
+						boolean insert = true;
+						for (int j = 0; insert && j <= 20 && lists.size() > 0 && pkt.totalLux() > 0.0001; j++) {
+							insert = false;
+
+							if (lists.size() == 1)
+								j = 20;
+
+							LuxStack p2 = pkt.copy();
+							if (j < 20)
+								p2 = p2.div(lists.size());
+
+							Iterator<Pos> it2 = lists.iterator();
+							while (it2.hasNext()) {
+								Pos q = it2.next();
+								if (inv.equals(q) || transmit.sameContainer(worldObj.getTileEntity(q.x, q.y, q.z))) {
+									pkt.extract(p2);
+									continue;
+								}
+
+								ILuxContainer t = ((ILuxContainer) worldObj.getTileEntity(q.x, q.y, q.z));
+
+								LuxStack k = t.insertLux(p2, Transfer.Perform);
+
+								pkt.extract(k);
+								transmit.extractLux(k, Transfer.Perform);
+								if (k.totalLux() < 0.001) {
+									it2.remove();
+								} else
+									insert = true;
+							}
+						}
+					}
+				}
+
+				if (b instanceof ILuxContainer) {
+					if (inv.equals(p) || transmit.sameContainer(tile)) {
+						iter.remove();
+						pkt = new LuxStack();
+
+						break;
+					}
+
+					LuxStack t = ((ILuxContainer) b).insertLux(pkt, Transfer.Perform);
+					pkt.extract(t);
+					transmit.extractLux(t, Transfer.Perform);
+
+				}
 
 			}
 
+			boolean empty = true;
+
+			for (int i = 0; i < pkt.lux.length; i++)
+				if (pkt.lux[i] > 0.0001F)
+					empty = false;
+
+			if (!empty)
+				transmit.extractLux(pkt, Transfer.Perform);
 		}
 
-		boolean empty = true;
-
-		for (int i = 0; i < pkt.lux.length; i++)
-			if (pkt.lux[i] > 0.0001F)
-				empty = false;
-
-		if (!empty)
-			transmit.extractLux(pkt, Transfer.Perform);
-
-		boolean changed = path.isChanged();
+		boolean changed = path != null && path.isChanged();
 
 		if (!changed) {
 			for (int i = 0; i < 8; i++)
@@ -140,12 +160,11 @@ public class TileEntityLuxLaser extends TileEntity implements ILaserTile {
 				}
 		}
 
-		laserLengths = newLaserLengths;
-
-		if (changed) {
+		if (changed)
 			this.worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
-		}
 
+		for (int i = 0; i < 8; i++)
+			laserLengths[i] = newLaserLengths[i];
 	}
 
 	public Packet getDescriptionPacket() {
--- java/com/rwtema/luxcraft/tiles/TileEntityLuxStorage.java
+++ java/com/rwtema/luxcraft/tiles/TileEntityLuxStorage.java
@@ -1,11 +1,19 @@
 package com.rwtema.luxcraft.tiles;
 
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.util.ForgeDirection;
+
+import com.rwtema.luxcraft.luxapi.ILaserTile;
 import com.rwtema.luxcraft.luxapi.LuxStack;
 
 public class TileEntityLuxStorage extends TileEntityLuxContainerBase {
 
 	public TileEntityLuxStorage() {
 		super(new LuxStack(1024));
+	}
+
+	public void onNeighbourChange() {
+		super.onNeighbourChange();
 	}
 
 }
